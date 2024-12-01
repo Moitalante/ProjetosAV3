b6b626a90ef26b42eac12368164085c9923196dc
Moitalante <carlopio1205@gmail.com>
2024-12-01 17:16:39 -0300

    mendes pq????????

--- python/vendas.py
+++ python/vendas.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from waitress import serve
 
 # Configuração do banco de dados
-DATABASE_URL = "mysql+pymysql://root:12345678@localhost:3306/vendas"
+DATABASE_URL = "mysql+pymysql://root:MekYvOHqGvOkEyrgvqDNYWYwdgEryzbm@mysql.railway.internal:3306/railway"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
 
